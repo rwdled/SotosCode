51961a539170179af8f440c4a484a5143d3dadd4
rwdled <as20694@student.ssttx.org>
2024-11-11 13:11:44 -0600

    new file:   notes4.py

---
+++ notes4.py
@@ -0,0 +1,16 @@
+#
+print(9*1)
+for i in range(1,101):
+    print(str(i*9) + str(" Hello"))
+
+    #for [name in local variable] aka i in [swquence]
+        #range() => creates a sequencde of numbers
+        #range(x) => creates sequence of numbers tp x-1
+        #range(x,y) => creates a sequence of numbers x to y-1
+        #range(x,y,2) +> creates a sequence of numbers gto, x to y-1. with z steps/increments
+        
+def test():
+    for a in range(1,101):
+        print(9*a)
+
+    
