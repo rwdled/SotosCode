df79a9d1bc935d84fdefd02a72b4f1ac47ee337c
rwdled <79921530+rwdled@users.noreply.github.com>
2024-12-09 12:33:49 -0600

    Update notes2.py

--- notes2.py
+++ notes2.py
@@ -1,9 +1,25 @@
 import string
 import time
-text = "On October 21, 1957, Queen Elizabeth II delivered a speech to the General Assembly of the United Nations to make a statement that was characterized by the Cold War and actions of decolonization. The purpose of her speech was to reassure the support of British citizens in their efforts to preserve peace and cooperation in the face of tensions on a global scale. An example of a speech that combines formal regal power with a personal idea to common human ideals is the one that is being given"
+import os
+text = "The first fully AI layerd two block chain for - by - LLM (Lotta little money) Technogly "
 
 temp = ''
 for ch in text:
+    for i in string.printable:
+      if i == ch or ch == '':
+        time.sleep(0.03)
+        print(temp + i)
+        temp +=ch
+        break
+    else:
+       time.sleep(0.02)
+       print(temp+i)
+
+os.system('clear')
+text2 = "proof of - entry scan system, broke - detection software Theromdynamic software one goraillen dps   "
+
+temp2 = ''
+for ch in text2:
     for i in string.printable:
       if i == ch or ch == '':
         time.sleep(0.03)
