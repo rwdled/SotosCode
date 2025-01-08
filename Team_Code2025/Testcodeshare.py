f7c0037d8519277b80d7f8f9a0c425f9c867b7df
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-08 13:15:39 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -1,2 +1,18 @@
+import turtle as tmuncher
 import random
-word = ["cat"]
+player = input('''Please select an option
+                1. Play
+                2. Login
+                3. View Leaderboard ''' )
+
+
+def newgame():
+   newttrt = tmuncher.Turtle(shape="turtle")
+   #newttrt.color()
+   newttrt.penup()
+   
+
+newgame()
+   
+   
+   
