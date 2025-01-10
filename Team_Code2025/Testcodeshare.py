52f93ee57538a0fa763c9838e012d4154a1fdb13
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-10 13:52:23 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -1,18 +1,31 @@
 import turtle as tmuncher
 import random
+import keyboard
+
 player = input('''Please select an option
                 1. Play
                 2. Login
                 3. View Leaderboard ''' )
 
 
+   
 def newgame():
    newttrt = tmuncher.Turtle(shape="turtle")
    #newttrt.color()
    newttrt.penup()
+   #Controls
+   
+   tmuncher.listen()
+   tmuncher.onkey(lambda: newttrt.left(180), "a")
+   tmuncher.onkey(lambda: newttrt.forward(100), "w")
+   tmuncher.onkey(lambda: newttrt.backward(100), "s")
+   tmuncher.onkey(lambda: newttrt.right(90), "d")
+         
+
+
+if player == "1":
+   newgame()
    
 
-newgame()
    
-   
-   
+tmuncher.done()
