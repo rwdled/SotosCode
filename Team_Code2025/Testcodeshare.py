9a8781cd51de22510563a314ae952fa3317ddfdc
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-15 13:14:23 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -10,31 +10,31 @@ player = input('''Please select an option
 
    
 def newgame():
-   #total_score   
-   def check_collision(check1, check2):
-      return check1.distance(check2) < 20
-   #create apple 
+   #list for turtle expansion
+   trtleextend = [tmuncher.Turtle(shape="square"), tmuncher.Turtle(shape="square")]
+   
+   newttrt = tmuncher.Turtle(shape="turtle")
+   while True:
+      trtleextend[1].setposition(-1, newttrt.ycor())
+     trtleextend[0].setposition(-1, newttrt.ycor())
+     break
+   newttrt.penup()
+   
    apple = tmuncher.Turtle(shape="circle")
    apple.color("red")
    apple.penup()
    apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
-
-   #check for collision 
-      
-   newttrt = tmuncher.Turtle(shape="turtle")
-   newttrt.penup()
-   # move then check for collision
+  
+   def check_collision(check1, check2):
+      return check1.distance(check2) < 20
    def point():
          if check_collision(newttrt, apple):
             apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
-            # if check_collision == True:
-            #     new_turtle = tmuncher.Turtle(shape="turtle")
-            #     new_turtle.penup()
-            #     new_turtle.setposition(newttrt.position())
-            #     tmuncher.clone()
-            #     newttrt.forward(25)             
-               
+            trtleextend.append(tmuncher.Turtle(shape="turtle"))
             
+            
+               
+        #controls    
    def move_and_check():
       newttrt.forward(25)
       point()
