f3001a91d363d5c9d212ebf98adc7ac2ec731e47
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-16 13:41:06 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -12,13 +12,12 @@ player = input('''Please select an option
 def newgame():
    #list for turtle expansion
    trtleextend = [tmuncher.Turtle(shape="square"), tmuncher.Turtle(shape="square")]
-   newttrt = tmuncher.Turtle(shape="turtle")
-   #while True:
-      #trtleextend[1].setposition(-1, newttrt.ycor())
-      #trtleextend[0].setposition(-1, newttrt.ycor())
-      #break
-   newttrt.penup()
    
+   newttrt = tmuncher.Turtle(shape="turtle")
+   newttrt.penup()
+   newttrt.color("black")
+
+   #apple
    apple = tmuncher.Turtle(shape="circle")
    apple.color("red")
    apple.penup()
@@ -26,24 +25,37 @@ def newgame():
   
    def check_collision(check1, check2):
       return check1.distance(check2) < 20
+   
+   def add_turtle():
+        new_turtle = tmuncher.Turtle(shape="square")
+        new_turtle.penup()
+        trtleextend.append(new_turtle)
+
+   def move_turtles():
+        for i in range(0, len(trtleextend) - 1,1):
+            trtleextend[i].goto(trtleextend[i - 1].position())
+        trtleextend[0].goto(newttrt.position())
+
    def point():
          if check_collision(newttrt, apple):
             apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
             trtleextend.append(tmuncher.Turtle(shape="turtle"))
-            
-            
-               
+            add_turtle()
+
         #controls    
    def move_and_check():
       newttrt.forward(25)
       point()
+      move_turtles()
+      tmuncher.ontime(move_and_check, 250)
+   
    tmuncher.listen()
+   move_and_check()
    tmuncher.onkey(lambda: newttrt.left(90), "a")
-   tmuncher.onkey(move_and_check, "w")
-   #tmuncher.onkey(lambda: newttrt.backward(25), "s")
    tmuncher.onkey(lambda: newttrt.right(90), "d")
-   point()
+   move_and_check()
    tmuncher.done()
+
 if player == "1":
    newgame()
   
