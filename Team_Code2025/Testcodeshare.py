8e515d359925217bd8ec27466f96f84fd8cdaf2e
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-27 13:50:40 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -3,13 +3,14 @@ import random
 import keyboard
 import time
 
+global points
+points = 0
 player = input('''Please select an option
                 1. Play
                 2. Login
                 3. View Leaderboard ''' )
 
 
-   
 def newgame():
    #list for turtle expansion
    trtleextend = [tmuncher.Turtle(shape="square"), tmuncher.Turtle(shape="square")]
@@ -33,7 +34,7 @@ def newgame():
         trtleextend.append(new_turtle)
 
    def move_turtles():
-        for i in range(0, len(trtleextend) - 1,1):
+        for i in range(len(trtleextend) - 1, 0, -1):
             trtleextend[i].goto(trtleextend[i - 1].position())
         trtleextend[0].goto(newttrt.position())
 
@@ -41,19 +42,30 @@ def newgame():
          if check_collision(newttrt, apple):
             apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
             add_turtle()
-
-        #controls    
-   def move_and_check():
-      if newgame ==  True:
-         newttrt.forward(25)
-         point()
-         move_turtles()
-         tmuncher.ontimer(move_and_check, 250)
+            points += 1
+            print("you have " + points)
+   while True:
+      move_turtles()
+      point()
+      tmuncher.update()
+      time.sleep(0.1)
+      newttrt.forward(20)
+      if newttrt.xcor() > 300 or newttrt.xcor() < -300 or newttrt.ycor() > 300 or newttrt.ycor() < -300:
+         print("ran into the wall, Game is ober")
+         break
+      for i in range(1, len(trtleextend)):
+         if check_collision(newttrt, trtleextend[i]):
+            print("ran into yourself, Game is ober")
+            break
+   #      controls    
+   
    
    tmuncher.listen()
    tmuncher.onkey(lambda: newttrt.left(90), "a")
    tmuncher.onkey(lambda: newttrt.right(90), "d")
    tmuncher.onkey(lambda: newttrt.forward(25), "w")
+   tmuncher.onkey(lambda: newttrt.backward(25), "s")
+   
    tmuncher.done()
 
 if player == "1":
