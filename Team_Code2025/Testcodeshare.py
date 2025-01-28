83817ff72bb29b33a35d368bc05847c85b655156
rwdled <79921530+rwdled@users.noreply.github.com>
2025-01-28 13:41:51 -0600

    modified:   Team_Code2025/Testcodeshare.py

--- Team_Code2025/Testcodeshare.py
+++ Team_Code2025/Testcodeshare.py
@@ -2,16 +2,16 @@ import turtle as tmuncher
 import random
 import keyboard
 import time
+import os
 
-global points
-points = 0
 player = input('''Please select an option
                 1. Play
                 2. Login
-                3. View Leaderboard ''' )
+''' )
 
 
 def newgame():
+   points = 0
    #list for turtle expansion
    trtleextend = [tmuncher.Turtle(shape="square"), tmuncher.Turtle(shape="square")]
    
@@ -23,7 +23,7 @@ def newgame():
    apple = tmuncher.Turtle(shape="circle")
    apple.color("red")
    apple.penup()
-   apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
+   apple.setposition(random.randint(-250, 249), random.randint(-250, 250))
   
    def check_collision(check1, check2):
       return check1.distance(check2) < 20
@@ -38,33 +38,33 @@ def newgame():
             trtleextend[i].goto(trtleextend[i - 1].position())
         trtleextend[0].goto(newttrt.position())
 
-   def point():
+   def point(z):
          if check_collision(newttrt, apple):
             apple.setposition(random.randint(-250, 250), random.randint(-250, 250))
             add_turtle()
-            points += 1
-            print("you have " + points)
+            #return z + 1
    while True:
+      tmuncher.listen()
+      tmuncher.onkey(lambda: newttrt.left(90), "a")
+      tmuncher.onkey(lambda: newttrt.right(90), "d")
+      tmuncher.onkey(lambda: newttrt.forward(25), "w")
+      tmuncher.onkey(lambda: newttrt.backward(25), "s")
       move_turtles()
-      point()
+      points = point(points)
       tmuncher.update()
-      time.sleep(0.1)
       newttrt.forward(20)
-      if newttrt.xcor() > 300 or newttrt.xcor() < -300 or newttrt.ycor() > 300 or newttrt.ycor() < -300:
-         print("ran into the wall, Game is ober")
+      time.sleep(0.1)
+      if newttrt.xcor() > 500 or newttrt.xcor() < -500 or newttrt.ycor() > 500 or newttrt.ycor() < -500:
+         print("ran into the wall, Game is ober. you had " + str(points) + " points")
          break
       for i in range(1, len(trtleextend)):
          if check_collision(newttrt, trtleextend[i]):
-            print("ran into yourself, Game is ober")
+            print("ran into yourself, Game is ober. you had " + str(points) + " points")
             break
-   #      controls    
+   #      controls    your a faggot :) - jordin&ben
    
    
-   tmuncher.listen()
-   tmuncher.onkey(lambda: newttrt.left(90), "a")
-   tmuncher.onkey(lambda: newttrt.right(90), "d")
-   tmuncher.onkey(lambda: newttrt.forward(25), "w")
-   tmuncher.onkey(lambda: newttrt.backward(25), "s")
+
    
    tmuncher.done()
 
