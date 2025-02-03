75253e52e9b7207bb12f7fc63930916025bebfe6
rwdled <79921530+rwdled@users.noreply.github.com>
2025-02-03 13:32:42 -0600

    modified:   PythonFunctions01.py 	new file:   javatest.java

---
+++ PythonFunctions01.py
@@ -0,0 +1,32 @@
+# Function: A resuable block the performs a specific task
+# function helps organize code, make it reusable, and reduce repetition
+
+'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+
+    '''
+name = "John"
+name = "Jane"
+# how to create a function
+def greet():
+    print("hello AP CSP")
+    print("Today, we will learn about functions")
+
+
+# how to call a function
+greet() #calling a function
+greet() #calling  it again
+#greet()
+
+#change it intp speific person greeting
+# function/merhod overloading
+def greet(name):
+    print("hello" + name)
+    print("Today, we will learn about functions")
+
+greet()
---
+++ javatest.java
@@ -0,0 +1,15 @@
+class javatest{
+    static void greet(String name, int age){ 
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args){
+        
+        greet("John", 20);
+        greet(20, "John");
+        System.out.println("Hello World");
+
+
+    
+    }
+}
